fced04962b63ea9dc3cc06b6eb828b2dc9ee7c92
Fernando Orozco <forozcomercado@gmail.com>
2022-09-08 14:46:35 -0500

    ADD links to navigate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddClientComponent } from './clients/add-client/add-client.component';
 import { ClientsComponent } from './clients/clients.component';
 
 const routes: Routes = [
   { path: 'clients', component: ClientsComponent },
+  { path: 'add-client', component: AddClientComponent },
   { path: '**', redirectTo: '/clients', pathMatch: 'full' },
 ];
 
