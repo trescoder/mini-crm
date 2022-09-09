32c25a01a8591600904692a3e7fb8f49cf8d3d7d
Fernando Orozco <forozcomercado@gmail.com>
2022-09-09 08:25:56 -0500

    UPDATE form validation do not use validators anymore

--- src/app/clients/add-client/add-client.component.ts
+++ src/app/clients/add-client/add-client.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ClientsService } from '../clients.service';
 
 @Component({
@@ -9,44 +9,66 @@ import { ClientsService } from '../clients.service';
 })
 export class AddClientComponent implements OnInit {
   myForm!: FormGroup;
+  isFormValid = false;
 
   constructor(private fb: FormBuilder, private clientService: ClientsService) {}
 
   ngOnInit(): void {
     this.myForm = this.fb.group({
-      name: [
-        '',
-        Validators.required,
-        Validators.minLength(2),
-        Validators.maxLength(20),
-      ],
-      last_name: [
-        '',
-        Validators.required,
-        Validators.minLength(2),
-        Validators.maxLength(20),
-      ],
-      email: ['', Validators.required, Validators.email],
-      tel: ['', Validators.required],
-      birthday: ['', Validators.required],
-      address: ['', Validators.required],
-      contact_type: ['Cliente', Validators.required],
-      origin: ['Landing page', Validators.required],
+      name: [''],
+      last_name: [''],
+      email: [''],
+      tel: [''],
+      birthday: [''],
+      address: [''],
+      contact_type: ['Cliente'],
+      origin: ['Landing page'],
     });
 
     this.myForm.valueChanges.subscribe(() => {
-      console.log(this.myForm.valid);
-      console.log(this.myForm.invalid);
+      this.validateForm();
     });
   }
 
   registerClient() {
-    this.clientService
-      .registerNewClient(this.myForm.value)
-      .subscribe((data: any) => {
+    this.clientService.registerNewClient(this.myForm.value).subscribe(
+      (data: any) => {
         if (data.success) {
           this.myForm.reset();
         }
-      });
+      },
+      (error: any) => {
+        alert(error.error.message);
+      }
+    );
+  }
+
+  validateForm() {
+    const { name, last_name, email, tel, birthday, address } =
+      this.myForm.value;
+    const current_date = new Date();
+    const birthday_year = new Date(birthday).getFullYear();
+
+    if (name.length < 2 || name.length >= 40) {
+      this.isFormValid = false;
+    } else if (last_name.length < 2 || last_name.length >= 40) {
+      this.isFormValid = false;
+    } else if (
+      !String(email)
+        .toLowerCase()
+        .match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )
+    ) {
+      this.isFormValid = false;
+    } else if (isNaN(Number(tel)) || tel.length !== 10) {
+      this.isFormValid = false;
+    } else if (current_date.getFullYear() - 18 <= birthday_year) {
+      this.isFormValid = false;
+    } else if (address.length < 10) {
+      this.isFormValid = false;
+    } else {
+      this.isFormValid = true;
+    }
   }
 }
