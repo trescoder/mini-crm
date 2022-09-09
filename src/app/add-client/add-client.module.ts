f6d9614a45f37fc3bf690b73d95a4ea8b1269473
Fernando Orozco <forozcomercado@gmail.com>
2022-09-09 16:38:56 -0500

    UPDATE cleaning modules

--- src/app/add-client/add-client.module.ts
+++ src/app/add-client/add-client.module.ts
@@ -4,10 +4,7 @@ import { AddClientComponent } from './add-client/add-client.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatChipsModule } from '@angular/material/chips';
+
 import { AddClientRoutingModule } from './add-client-routing.module';
 
 @NgModule({
@@ -17,10 +14,6 @@ import { AddClientRoutingModule } from './add-client-routing.module';
     ReactiveFormsModule,
     AddClientRoutingModule,
     MatInputModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatChipsModule,
   ],
 })
 export class AddClientModule {}
--- src/app/clients/clients.module.ts
+++ src/app/clients/clients.module.ts
@@ -1,25 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatChipsModule } from '@angular/material/chips';
 import { ClientsRoutingModule } from './clients-routing.module';
 import { ClientsComponent } from './clients-list/clients.component';
 import { ClientComponent } from './client/client.component';
 
 @NgModule({
   declarations: [ClientsComponent, ClientComponent],
-  imports: [
-    CommonModule,
-    MatInputModule,
-    ClientsRoutingModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatCheckboxModule,
-    MatChipsModule,
-  ],
+  imports: [CommonModule, ClientsRoutingModule],
 })
 export class ClientsModule {}
