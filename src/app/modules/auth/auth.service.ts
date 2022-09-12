6b5ebcc083489740403f22d63db4944052e1aa87
Fernando Orozco <forozcomercado@gmail.com>
2022-09-12 12:18:49 -0500

    ADD auth service

---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Login } from 'src/app/interfaces/login';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  readonly url = environment.apiURL + '/user';
+  public defaultHeaders = new HttpHeaders();
+  constructor(private http: HttpClient) {}
+
+  login(body: Login) {
+    let headers = this.defaultHeaders;
+    headers = headers.set('Content-Type', 'application/json');
+    return this.http.post<{ token: string }>(this.url + '/login', body, {
+      headers,
+    });
+  }
+}
