1d22722f394fc5651e003542ff3e14738dcebdb0
Fernando Orozco <forozcomercado@gmail.com>
2022-09-12 22:45:47 -0500

    refactoring app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './modules/auth/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: '/auth',
+    pathMatch: 'full',
+  },
+  {
+    path: 'auth',
     loadChildren: () =>
-      import('./modules/home/home.module').then((h) => h.HomeModule),
+      import('./modules/login/login.module').then((l) => l.LoginModule),
+  },
+  // the clients view will be outside the home component
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('./modules/clients/clients.module').then((c) => c.ClientsModule),
+    canActivate: [AuthGuard],
   },
   { path: '**', redirectTo: '/', pathMatch: 'full' },
 ];
